69ecc60fee791820ef84c2bdfa04bdb507ea4ca6
Kuya Loy <robintapiru0894@gmail.com>
2025-01-27 00:20:02 +0400

    const Assertions

---
+++ src/data.ts
@@ -0,0 +1,20 @@
+export const initialTickets = [
+  {
+    id: "1",
+    title: "Ticket 1",
+    content: "this is the first ticket",
+    status: "DONE" as const,
+  },
+  {
+    id: "2",
+    title: "Ticket 2",
+    content: "this is the second ticket",
+    status: "OPEN" as const,
+  },
+  {
+    id: "3",
+    title: "Ticket 3",
+    content: "this is the second ticket",
+    status: "IN_PROGRESS" as const,
+  },
+];
